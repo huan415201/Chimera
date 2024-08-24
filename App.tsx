1a38d5754f5aa5aa5e594c88a827ee59d3d9d2b9
huan415201 <huan415201@gmail.com>
2024-08-24 16:26:35 +0700

    feat: implement navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import Navigator from './app/navigator';
 import { persistor, store } from './app/redux';
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <View />
+        <Navigator />
       </PersistGate>
     </Provider>
   );
